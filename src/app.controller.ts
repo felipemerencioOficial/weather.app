c3afae18d567dbc5169c9a949e2b5b94fca8a00c
Felipe Merêncio <soumerencio@gmail.com>
2024-08-08 11:57:13 -0300

    ✨: Add weather endpoint to app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('weather/:city')
+  getHello(@Param('city') city: string) {
+    // return this.appService.getHello();
+    return `The weather in ${city} is 72 degrees`;
   }
 }

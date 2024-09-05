b5d42ad3ee300efbdf374924c8af1a238aa3a4fa
Felipe Merêncio <soumerencio@gmail.com>
2024-09-05 10:28:23 -0300

    ✨ : Add api for geolocation query

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,11 @@ import { HttpService } from '@nestjs/axios';
 import { map } from 'rxjs/operators';
 import { lastValueFrom } from 'rxjs';
 const { format, startOfHour } = require('date-fns');
+import axios from 'axios';
+import * as dotenv from 'dotenv';
+
+// Carrega as variáveis de ambiente do arquivo .env
+dotenv.config();
 
 @Injectable()
 export class AppService {
@@ -20,28 +25,48 @@ export class AppService {
     return hourly.time.indexOf(targetHour);
   }
 
-  // TODO: Remover os parametros chubados e implementar a busca por cidade
+  // Função para obter latitude e longitude a partir do nome da cidade e estado
+  async getCoordinates(city: string, state: string): Promise<{ latitude: number, longitude: number }> {
+    const apiKey = process.env.OPENCAGE_API_KEY; // Substitua pela sua chave de API do OpenCage
+    const apiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(city + ', ' + state)}&key=${apiKey}`;
+
+    try {
+      const response = await axios.get(apiUrl);
+      const data = response.data;
+
+      if (data.results && data.results.length > 0) {
+        const { lat, lng } = data.results[0].geometry;
+        return { latitude: lat, longitude: lng };
+      } else {
+        throw new Error('No results found');
+      }
+    } catch (error) {
+      throw new Error(`Error fetching coordinates: ${error.message}`);
+    }
+  }
+
+  // TODO: Remover time zone fixo
+  // TODO: Remover campo de country
   async getWeatherForecast(country: string, state: string, city: string): Promise<string> {
-    // const apiUrl = `https://api.example.com/weather?country=${country}&state=${state}&city=${city}`;
-    const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=-22.9056&longitude=-47.0608&hourly=temperature_2m&timezone=America%2FSao_Paulo`;
+    // const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=-22.9056&longitude=-47.0608&hourly=temperature_2m&timezone=America%2FSao_Paulo`;
+    // Obtém as coordenadas da cidade e estado
+    const { latitude, longitude } = await this.getCoordinates(city, state);
+    const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&timezone=America%2FSao_Paulo`;
     try {
       const response = await lastValueFrom(
         this.httpService.get(apiUrl).pipe(
           map(response => response.data)
         )
       );
-      console.log(response);
 
       // Obtém a hora cheia atual
       const currentHour = this.getCurrentHour();
-      console.log(`Current Hour: ${currentHour}`);
 
       // Encontra o índice da hora específica
       const hourIndex = this.findHourIndex(response.hourly, currentHour.toString());
       if (hourIndex !== -1) {
         const temperature = response.hourly.temperature_2m[hourIndex];
-        console.log(`Temperature at 2024-08-29T10:00: ${temperature}°C`);
-        return `Agora são ${temperature}°C em ${city}`;
+        return `Atualmente são ${temperature}°C em ${city}`;
       } else {
         return 'Hour not found in the response data';
       }
